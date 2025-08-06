5cf39fc50112ac8efafb58e3f1e0e04c5df29dca
Davide-Ficocelli <davide.ficocelli.dev@gmail.com>
2025-08-06 14:44:34 +0200

    Added comments documentation

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,20 @@
-// Selecting the DOM elements
+// Selecting the player element in the DOM
 const player = document.getElementById("player");
-console.log(player);
 
 // Creating a function to make the player jump
+// The player can jump by pressing space or the arrow up like in the official game
+// It is also possible to jump by clicking anywhere on the screen, useful for mobile users
 function jump(e) {
+  // Preventing scrolling when pressing space or arrow up
   e.preventDefault();
+
+  // Converting the player class list to an array for easier inspection
   const playerClasses = Array.from(player.classList);
+
+  // Preventing animation resetting by spamming the jump button
   if (playerClasses.includes("jump")) return;
+
+  // Establishing valid inputs and impeding jumping if they aren't valid
   const isValidInput =
     e.key === " " || e.key === "ArrowUp" || e.type === "click";
 
@@ -15,7 +23,7 @@ function jump(e) {
   // Adding the 'jump' class to the player
   player.classList.add("jump");
 
-  // Removing the 'jump' class after 500 milliseconds
+  // Removing the 'jump' class after 500 milliseconds (the same time as the animation duration in CSS in the "jump" class)
   setTimeout(() => {
     player.classList.remove("jump");
   }, 500);
